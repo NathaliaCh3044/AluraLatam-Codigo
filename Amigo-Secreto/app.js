9fe5cf05fea55c76f9e0ba92c6b043919ffd57f3
NathaliaCh3044 <chivatalisbeth@gmail.com>
2025-09-18 19:50:53 -0500

    Implementar gestión inicial de amigos (array, agregar y limpiar input)
    - Definir el array para guardar los amigos.
    - Crear la función agregarAmigo() que valida y agrega nombres.
    - Crear la función limpiarCaja() para resetear el input

--- Amigo-Secreto/app.js
+++ Amigo-Secreto/app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Lista de amigos
+let listaGuardarAmigos = [];
+
+// Limpia la caja de texto
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+// Agregar un nuevo amigo
+// Se usa trim() para eliminar espacios en blanco al inicio y final del texto
+function agregarAmigo() {
+    let nombreUsuario = document.getElementById('amigo').value.trim();
+
+    if (nombreUsuario === "") {
+        alert("Por favor, escribe un nombre válido.");
+    } else {
+        listaGuardarAmigos.push(nombreUsuario);
+        console.log(listaGuardarAmigos)
+        limpiarCaja();
+    }
+}
